42f579c430164819db73073bfbeacd957e320dc2
tzach <tzach@upstream.auto>
2022-06-19 10:36:46 +0300

    adding additional test ip addresses

--- src/main/kotlin/IPSecurityMain.kt
+++ src/main/kotlin/IPSecurityMain.kt
@@ -1,8 +1,8 @@
 import cidr.security.*
 
 fun main(args: Array<String>) {
-    val blockIpRange = listOf("172.16.0.1/23", "172.16.0.1/8", "192.16.8.1/4")
-    val ipRequestList = listOf("172.16.0.1", "216.254.128.0")
+    val blockIpRange = listOf("172.16.0.1/23", "172.16.0.1/8", "192.16.8.1/4","192.168.0.0/23")
+    val ipRequestList = listOf("172.16.0.1", "216.254.128.0","172.16.0.0")
     val blackList = CidrRequests(blockIpRange)
     println(blackList.unauthorizedIpAddresses)
     ipRequestList.forEach {
